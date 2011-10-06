2540e3204f75acb19cb38665a448023a02bf5ab7
Mojo Media Labs <sean@teamrgc.com>
2011-10-06 13:25:12 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BmiAssessment::Application.config.secret_token = '6342450767cff255ef0c79f3dc671a98d32a79bf1aa766a2d27e5bf8bff328e08290b1123e07ec6f1cb64f00611e942c2d9a49e641fc63e6b7abbe26ed48cb76'
